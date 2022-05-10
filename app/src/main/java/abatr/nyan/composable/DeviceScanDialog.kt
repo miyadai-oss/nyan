4eea98da2a1e20110eb5c187a631f68eeb06bfd7
Masahiro MIYADAI <abatarouzamurai@gmail.com>
2022-05-10 11:07:28 +0900

    Dialog progress display implemented. Next, implement the device detection process.

--- app/src/main/java/abatr/nyan/composable/DeviceScanDialog.kt
+++ app/src/main/java/abatr/nyan/composable/DeviceScanDialog.kt
@@ -1,30 +1,50 @@
 package abatr.nyan.composable
 
 import abatr.nyan.R
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import android.bluetooth.BluetoothDevice
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DeviceScanDialog(isDialogShown: Boolean, onDismissRequest: () -> Unit) {
     if (isDialogShown) {
-        // TODO Implement a dialog that detects and displays devices.
+        val devices = remember { mutableStateListOf<BluetoothDevice>() }
+
         AlertDialog(
             onDismissRequest = {
                 onDismissRequest()
             },
             title = {
-                Text(text = stringResource(id = R.string.select_device))
+                Text(text = stringResource(R.string.select_device))
+            },
+            text = {
+                Column {
+                    if (devices.isEmpty()) {
+                        Text(text = stringResource(R.string.no_devices))
+                    } else {
+                        // TODO When devices are detected, they are listed.
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    LinearProgressIndicator()
+                }
             },
             confirmButton = {
                 TextButton(onClick = {
                     onDismissRequest()
                 }) {
-                    Text(text = stringResource(id = android.R.string.cancel))
+                    Text(text = stringResource(android.R.string.cancel))
                 }
             }
         )
+        LaunchedEffect(true) {
+            // TODO Implement the device detection process.
+        }
     }
 }
